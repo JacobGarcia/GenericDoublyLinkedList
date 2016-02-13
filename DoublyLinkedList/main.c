3f32fdf8208b49e6c8801c40a4f9ae15d7a66e9b
Jacob Garcia <rock2494@hotmail.com>
2016-02-12 19:28:02 -0600

    Initial Commit

---
+++ DoublyLinkedList/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DoublyLinkedList
+//
+//  Created by Jacob on 2/12/16.
+//  Copyright © 2016 UmbrellaCorp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
